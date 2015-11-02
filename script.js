80561edcdaddac4dc75baeca00902eeff3a843fb
jerrypupu111 <pupu1416@yahoo.com.tw>
2015-11-03 01:25:33 +0800

    fb aped

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
 $('.top-text, .bottom-text').on('input', function(){
 		  generate();
@@ -20,7 +18,20 @@ $('.top-text, .bottom-text').on('input', function(){
 	});
 	$('#gen-btn').click(function()
 	{
-		render();
+		var img_data = render();
+
+		FB.api(
+		    "/me/photos",
+		    "POST",
+		    {
+		        "source": img_data
+		    },
+		    function (response) {
+		      if (response && !response.error) {
+		        /* handle the result */
+		      }
+		    }
+		);
 	});
 
 	function newBottom()
@@ -50,7 +61,6 @@ $('.top-text, .bottom-text').on('input', function(){
 		}
 		
 		
-		
 	  $('.bottom-area .bottom-text').each(function(index){
 			//console.log($(this).val());
 			var bottom_text = $(this).val();
@@ -64,8 +74,54 @@ $('.top-text, .bottom-text').on('input', function(){
 	}
 	function render()
 	{
-		var appid = '145634995501895';
-		var url = canvas.toDataURL('image/jpg');
-		window.open('https://www.facebook.com/dialog/share?app_id='+appid+'&display=popup&href='+url+'&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer');
+		//var appid = '145634995501895';
+		var data = canvas.toDataURL('image/png');
+		try{
+    	blob = dataURItoBlob(data);
+		}catch(e){console.log(e);}
+
+		var fd = new FormData();
+		fd.append("access_token",authToken);
+		fd.append("source", blob);
+		fd.append("message","Photo Text");
+
+		try {
+		    $.ajax({
+		        url: "https://graph.facebook.com/me/photos?access_token=" + authToken,
+		        type: "POST",
+		        data: fd,
+		        processData: false,
+		        contentType: false,
+		        cache: false,
+		        success: function (data) {
+		            console.log("success " + data);
+		            $("#poster").html("Posted Canvas Successfully");
+		        },
+		        error: function (shr, status, data) {
+		            console.log("error " + data + " Status " + shr.status);
+		        },
+		        complete: function () {
+		            console.log("Posted to facebook");
+		        }
+		    });
+
+		} catch (e) {
+		    console.log(e);
+		}
+			
+		//var open_url ='https://www.facebook.com/dialog/share?app_id='+appid+'&display=popup&href='+dataURLToBlob(url)+'&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer';
+		//window.open(open_url);
+		//console.log(open_url);
 	}
+
+	function dataURItoBlob(dataURI) {
+	var byteString = atob(dataURI.split(',')[1]);
+	var ab = new ArrayBuffer(byteString.length);
+	var ia = new Uint8Array(ab);
+	for (var i = 0; i < byteString.length; i++) {
+	    ia[i] = byteString.charCodeAt(i);
+	}
+	return new Blob([ab], { type: 'image/png' });
+	}
+
 });
